2c7fd69213c889fa31968cdb7125b57aff781320
Xi Xu <xixu@andrew.cmu.edu>
2024-02-02 11:37:18 -0500

    change interpolate default parameter to bicubic and align corners

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3846,8 +3846,8 @@ def interpolate(  # noqa: F811
     input: Tensor,
     size: Optional[List[int]] = None,
     scale_factor: Optional[float] = None,
-    mode: str = "nearest",
-    align_corners: Optional[bool] = None,
+    mode: str = "bicubic",
+    align_corners: Optional[bool] = True,
     recompute_scale_factor: Optional[bool] = None,
     antialias: bool = False,
 ) -> Tensor:  # noqa: F811
